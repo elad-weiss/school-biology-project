5c3e5bf93db44b1391ba63d0bbae2cca54cb74ff
elad-weiss <elad_Weiss@hotmail.com>
2021-02-20 23:14:01 +0200

    switched placement method from .grid() to .place()

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ import json
 
 root = Tk()
 
-#sets app name and icon
+#sets app name, icon and size
+root.geometry("1000x600")
 root.title("Smart To Do")
 #TODO: create and place app icon
 
 #sets personal greeting message in top left corner
 user_greeting_lbl = Label(text=f"Hello {getpass.getuser()}", font="Helvetica 15", padx=15, pady=30)
-user_greeting_lbl.grid(column=0, row=0)
+user_greeting_lbl.place(relx=0.02, rely=0.01)
 
 #gets all the lists names
 with open("lists.json") as f:
     lists = json.load(f)
-
-#a spacing between the greeting and all the lists buttons
-Label(root, text=" ", padx=15, pady=40).grid(column=0, row=1)
 
 #initiats a list that contains all lists names
 user_lists = []
@@ -29,22 +27,27 @@ for list_name in lists:
 
 #places each button from the user_lists list on the screen
 for i in range(len(user_lists)):
-    user_lists[i].grid(column=0, row=i+2)
+    y_pos = i/10+0.25
+    user_lists[i].place(relx=0.02, rely=y_pos)
 
-user_lists_len = len(user_lists)
+user_lists_len = (len(user_lists)+1)/10+0.1
 
 #a dividing line between the lists buttons and the add list button
-tkinter.ttk.Separator(root, orient=HORIZONTAL).grid(column=0, row=user_lists_len+3,
-                                                    columnspan=1, sticky="we", pady=10)
+tkinter.ttk.Separator(root, orient=HORIZONTAL).place(relx=0.01, rely=user_lists_len+0.1, relwidth=0.15, relheight=1)
 
 #creats and places on the screen the "add list" button
 add_list_btn = Button(root, text="+ Add List", font="Helvetica 12 bold", padx=15, pady=30, borderwidth=0)
-add_list_btn.grid(column=0, row=user_lists_len+4)
+add_list_btn.place(relx=0.02, rely=user_lists_len+0.12)
 
 #creats and places on the screen the "delete list" button
 delete_list_btn = Button(root, text="- Delete List", font="Helvetica 12 bold",
                          padx=15, pady=20, borderwidth=0)
-delete_list_btn.grid(column=0, row=user_lists_len+5)
+delete_list_btn.place(relx=0.02, rely=user_lists_len+0.22)
+
+
+#separator between the main part of the app and the side bar
+main_separator = tkinter.ttk.Separator(root, orient=VERTICAL)
+main_separator.place(relx=0.2, rely=0, relwidth=0.2, relheight=1)
 
 
 root.mainloop()
