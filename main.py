37fa574a54005e1a4d3d49daff2a6c632379bf03
elad-weiss <elad_Weiss@hotmail.com>
2021-02-28 19:16:38 +0200

    :construction: Work in progress: add main screen gui

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from tkinter import *
 import tkinter.ttk
 import getpass
 import json
+import functionalities
+
+#a variable that saves the current list that is on the screen
+curr_list = ""
+
+#A function to change the current list
+def change_list(name):
+    global curr_list
+    print(f"change list name: {name}")
+    curr_list = name
+    print(f"change curr_list: {curr_list}")
+    functionalities.load_list(name, root)
+    list_title.config(text=curr_list)
 
 root = Tk()
 
@@ -23,11 +36,14 @@ user_lists = []
 
 #append each name from the json file to the user_lists list as a button object
 for list_name in lists:
+    print(f"checkpoint A: {list_name}")
     user_lists.append(Button(root, text=list_name, font="Helvetica 14", padx=15, pady=20, borderwidth=0))
 
 #places each button from the user_lists list on the screen
 for i in range(len(user_lists)):
     y_pos = i/10+0.25
+    print("list text:", user_lists[i].cget("text"))
+    user_lists[i].config(command=lambda name_index = user_lists[i].cget("text"): change_list(name_index))
     user_lists[i].place(relx=0.02, rely=y_pos)
 
 user_lists_len = (len(user_lists)+1)/10+0.1
@@ -48,6 +64,19 @@ delete_list_btn.place(relx=0.02, rely=user_lists_len+0.22)
 #separator between the main part of the app and the side bar
 main_separator = tkinter.ttk.Separator(root, orient=VERTICAL)
 main_separator.place(relx=0.2, rely=0, relwidth=0.2, relheight=1)
+
+#sets curr_list variable
+with open("lists.json") as f:
+    lists = json.load(f)
+    if curr_list == "": curr_list = next(iter(lists))
+    else: curr_list = curr_list
+
+#displays the list title
+list_title = Label(root, text=curr_list, font="Helvetica 16 bold", padx=15, pady=30)
+list_title.place(relx=0.55, rely=0.03)
+
+#loads all the task bars on screen
+functionalities.load_list(curr_list, root)
 
 
 root.mainloop()
