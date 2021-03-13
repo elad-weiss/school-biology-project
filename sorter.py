3c1868c1163742449e241cd2583cf78304c8a3bd
elad-weiss <elad_Weiss@hotmail.com>
2021-03-13 21:37:07 +0200

    :package: NEW: added json file that saves the sorting data

--- sorter.py
+++ sorter.py
@@ -1,10 +1,12 @@
-def sort_a():
+import json
+
+def sort_a(list_name):
     print("sorting sort a")
 
 
-def sort_b():
+def sort_b(list_name):
     print("sorting sort b")
 
 
-def sort_c():
+def sort_c(list_name):
     print("sorting sort c")
